ec01d724de3b4bc19bab72fe0d445c84a2e3b585
kastldratza <kastldratza@protonmail.com>
2025-05-07 12:33:00 +0200

    Update set-date.ps1

--- set-date.ps1
+++ set-date.ps1
@@ -2,7 +2,7 @@ $CurrentDate = Get-Date
 $CountdownDate = $CurrentDate.AddHours(8).ToString("yyyy-MM-ddTHH:mm:ss")
 $HtmlFile = "countdown.html"
 $htmlContent = Get-Content $HtmlFile
-$htmlContent = $htmlContent -replace 'const countdownDate = new Date\("[^"]*"\)\.getTime\(\);', 'const countdownDate = new Date("' + $CountdownDate + '").getTime();'
+$htmlContent = $htmlContent -replace 'const countdownDate = new Date\("[^"]*"\)\.getTime\(\);', "const countdownDate = new Date(`"$CountdownDate`").getTime();"
 Set-Content $HtmlFile $htmlContent
 Write-Host "HTML file updated with countdown date: $CountdownDate"
 
